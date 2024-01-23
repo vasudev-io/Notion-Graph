2c831680e161f2f46cf50c7d34d2e9093c6eb133
vasudev-io <ee20947@bristol.ac.uk>
2024-01-23 05:05:53 +0000

    vercel app req final fix 2

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template, request
 import notion_graph.parser  # Import your script here
 import subprocess
 
-app = Flask(__name__, template_folder="templates_new")
+app = Flask(__name__, template_folder=".")
 
 @app.route('/', methods=['GET', 'POST'])
-
 def home():
     if request.method == 'POST':
         command = ['pdm', 'run', 'start', '-p', 'ed228bfa2ece43f0a34a0aa95985a501', '-t', 'secret_37RsVuHNkIx573FXkg4geRqXcmiBSlBH45QxmVEXIQ9', '-o', './graph_out.html']
@@ -20,6 +19,17 @@ def home():
 
     return render_template('index.html')
 
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    command = ['pdm', 'run', 'start', '-p', 'ed228bfa2ece43f0a34a0aa95985a501', '-t', 'secret_37RsVuHNkIx573FXkg4geRqXcmiBSlBH45QxmVEXIQ9', '-o', './graph_out.html']
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    with open('graph_out.html') as f:
+        graph_output = f.read()
+
+    return graph_output, 200
 
 
 if __name__ == '__main__':
