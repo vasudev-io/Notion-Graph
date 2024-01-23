970b7bb3374cb1562e797608952397c18d0c247d
vasudev-io <ee20947@bristol.ac.uk>
2024-01-23 04:51:22 +0000

    vercel app req final fix

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, request
 import notion_graph.parser  # Import your script here
 import subprocess
 
-app = Flask(__name__, template_folder="/templates_new")
+app = Flask(__name__, template_folder="templates_new")
 
 @app.route('/', methods=['GET', 'POST'])
 
 def home():
     if request.method == 'POST':
-        command = ['pdm run start -p ed228bfa2ece43f0a34a0aa95985a501 -t secret_37RsVuHNkIx573FXkg4geRqXcmiBSlBH45QxmVEXIQ9 -o ./graph_out.html']  # Replace with your command
+        command = ['pdm', 'run', 'start', '-p', 'ed228bfa2ece43f0a34a0aa95985a501', '-t', 'secret_37RsVuHNkIx573FXkg4geRqXcmiBSlBH45QxmVEXIQ9', '-o', './graph_out.html']
         process = subprocess.Popen(command, stdout=subprocess.PIPE)
         output, error = process.communicate()
 
@@ -20,5 +20,9 @@ def home():
 
     return render_template('index.html')
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    print(app.template_folder)
