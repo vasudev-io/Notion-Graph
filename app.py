6444e50605b3950553d1c4e0ef6745bdcd9f9d9e
vasudev-io <ee20947@bristol.ac.uk>
2024-01-23 04:41:11 +0000

    vercel app req fix 5

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 import notion_graph.parser  # Import your script here
 import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/templates")
 
 @app.route('/', methods=['GET', 'POST'])
 
